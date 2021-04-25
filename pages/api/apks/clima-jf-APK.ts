48eb347b846aba709058edf43d467b875acf39f6
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-04-25 00:39:28 -0300

    Novamente process.cwd()

--- pages/api/apks/clima-jf-APK.ts
+++ pages/api/apks/clima-jf-APK.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import fs from 'fs'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const filePath = path.join('apks','clima-jf-APK.apk')
+  const filePath = path.join(process.cwd(),'apks','clima-jf-APK.apk')
   res.setHeader('accept-ranges','bytes')
   res.setHeader('content-disposition','attachment; filename=clima-jf-APK.apk')
   res.setHeader('content-type','application/vnd.android.package-archive')
