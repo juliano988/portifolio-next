6bb556abc93d2ba129e13fdbb615646e35e6ae08
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-04-25 13:45:13 -0300

    APK zipado

--- pages/api/apks/clima-jf-APK.ts
+++ pages/api/apks/clima-jf-APK.ts
@@ -4,9 +4,9 @@ import path from 'path';
 import fs from 'fs'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const filePath = path.join(process.cwd(),'apks','clima-jf-APK.apk')
+  const filePath = path.join(process.cwd(),'apks','clima-jf-APK.zip')
   res.setHeader('accept-ranges','bytes')
-  res.setHeader('content-disposition','attachment; filename=clima-jf-APK.apk')
-  res.setHeader('content-type','application/vnd.android.package-archive')
+  res.setHeader('content-disposition','attachment; filename=clima-jf-APK.zip')
+  res.setHeader('content-type','application/zip')
   res.send(fs.readFileSync(filePath,null))
 }
