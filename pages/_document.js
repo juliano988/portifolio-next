aa44399127c4b69f91f00bd4dfe31c00829285a3
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-04-27 11:52:39 -0300

    Adição da meta tag do Google Search Console

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="google-site-verification" content="6LmGEnemTZUVKxhgbyfSah8BAXH8ezjIKk3TgQIMEhE" />
+        </Head>
         <body>
           <Main />
           <NextScript />
