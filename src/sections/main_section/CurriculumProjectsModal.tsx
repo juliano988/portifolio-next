ea638c8d074a5e2354c29cd2e6871e89bd1a5049
Júlio Faria <julio-pinheiroo@hotmail.com>
2024-01-16 18:52:58 -0300

    Bug na cor dos botões solucionado

--- src/sections/main_section/CurriculumProjectsModal.tsx
+++ src/sections/main_section/CurriculumProjectsModal.tsx
@@ -10,9 +10,10 @@ import FccCurSect5 from '../fcc_cur_sec_5';
 
 export default function CurriculumProjectsModal(props: { state: boolean, setState: React.Dispatch<React.SetStateAction<boolean>> }) {
 
-  const setselectedSection = useContext(SelectedSectionContext)
+  const setselectedSection = useContext(SelectedSectionContext);
 
   function handleClickButtons(secId: string) {
+
     switch (secId) {
       case '#fcc_cur_sec_1': setselectedSection(<FccCurSect1 />); break;
       case '#fcc_cur_sec_2': setselectedSection(<FccCurSect2 />); break;
@@ -21,50 +22,64 @@ export default function CurriculumProjectsModal(props: { state: boolean, setStat
       case '#fcc_cur_sec_5': setselectedSection(<FccCurSect5 />); break;
       default: break;
     }
+
     props.setState(false);
     setTimeout(() => window.location.href = secId, 500);
+
   }
 
   return (
+
     <Modal scrollable={true} show={props.state} onHide={() => props.setState(false)}>
+
       <Modal.Header closeButton>
         <Modal.Title>freeCodeCamp Curriculum Projects</Modal.Title>
       </Modal.Header>
-      
-        <Modal.Body>
-          <div className={styles.modal_buttons_div}>
-            <div className={styles.modal_buttons_description_div}>
-              <p>In this certification, I've learned how to build webpages with <strong>HTML</strong> and <strong>CSS</strong> that respond to different screen sizes.</p>
-              <Button onClick={() => handleClickButtons("#fcc_cur_sec_1")} variant="primary">Responsive Web Design</Button>
-              <hr />
-            </div>
-            <div className={styles.modal_buttons_description_div}>
-              <p>Having learned <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong>, this certification taught how to build webpages with modern front-end libraries, like:
+
+      <Modal.Body>
+
+        <div className={styles.modal_buttons_div}>
+
+          <div className={styles.modal_buttons_description_div}>
+            <p>In this certification, I've learned how to build webpages with <strong>HTML</strong> and <strong>CSS</strong> that respond to different screen sizes.</p>
+            <Button onClick={() => handleClickButtons("#fcc_cur_sec_1")} className='bg-blue-700' variant="primary">Responsive Web Design</Button>
+            <hr />
+          </div>
+
+          <div className={styles.modal_buttons_description_div}>
+            <p>Having learned <strong>HTML</strong>, <strong>CSS</strong> and <strong>JavaScript</strong>, this certification taught how to build webpages with modern front-end libraries, like:
               <strong> Bootstrap</strong>, <strong>jQuery</strong>, <strong>SASS</strong>, <strong>React</strong>, <strong>Redux</strong>.<br />
               All the certification projects were made using <strong>React</strong>.</p>
-              <Button onClick={() => handleClickButtons("#fcc_cur_sec_2")} variant="secondary">Front-end Libraries</Button>
-              <hr/>
-            </div>
-            <div className={styles.modal_buttons_description_div}>
-              <p>Working on back-end, this certification taught how to build APIs and microservices using <strong>Node.js</strong> and <strong>npm, </strong>
-              and also how to work with <strong>MongoDB</strong> using <strong>Mongoose</strong> library.</p>
-              <Button onClick={() => handleClickButtons("#fcc_cur_sec_3")} variant="success">APIs and Microservices</Button>
-              <hr/>
-            </div>
-            <div className={styles.modal_buttons_description_div}>
-              <p>In this certification, I've learned how to build charts, graphs, and maps to present different types of data with the <strong>D3.js</strong> library.<br />
-              All the certification projects were made using <strong>Next.js</strong> framework.</p>
-              <Button onClick={() => handleClickButtons("#fcc_cur_sec_4")} variant="warning">Data Visualization</Button>
-              <hr/>
-            </div>
-            <div className={styles.modal_buttons_description_div}>
-              <p>Focousing on software quality, this certification taught how to create unit tests on server side with <strong>Chai</strong>.</p>
-              <Button onClick={() => handleClickButtons("#fcc_cur_sec_5")} variant="danger">Quality Assurance</Button>
-              <hr/>
-            </div>
+            <Button onClick={() => handleClickButtons("#fcc_cur_sec_2")} className='bg-gray-600' variant="secondary">Front-end Libraries</Button>
+            <hr />
           </div>
-        </Modal.Body>
-      
+
+          <div className={styles.modal_buttons_description_div}>
+            <p>Working on back-end, this certification taught how to build APIs and microservices using <strong>Node.js</strong> and <strong>npm, </strong>
+              and also how to work with <strong>MongoDB</strong> using <strong>Mongoose</strong> library.</p>
+            <Button onClick={() => handleClickButtons("#fcc_cur_sec_3")} className='bg-green-600' variant="success">APIs and Microservices</Button>
+            <hr />
+          </div>
+
+          <div className={styles.modal_buttons_description_div}>
+            <p>In this certification, I've learned how to build charts, graphs, and maps to present different types of data with the <strong>D3.js</strong> library.<br />
+              All the certification projects were made using <strong>Next.js</strong> framework.</p>
+            <Button onClick={() => handleClickButtons("#fcc_cur_sec_4")} className='bg-yellow-500' variant="warning">Data Visualization</Button>
+            <hr />
+          </div>
+
+          <div className={styles.modal_buttons_description_div}>
+            <p>Focusing on software quality, this certification taught how to create unit tests on server side with <strong>Chai</strong>.</p>
+            <Button onClick={() => handleClickButtons("#fcc_cur_sec_5")} className='bg-red-600' variant="danger">Quality Assurance</Button>
+            <hr />
+          </div>
+
+        </div>
+
+      </Modal.Body>
+
     </Modal>
+
   )
+
 }
--- src/sections/main_section/DevelopmentChallengesModal.tsx
+++ src/sections/main_section/DevelopmentChallengesModal.tsx
@@ -21,27 +21,32 @@ export default function DevelopmentChallengesModal(props: { state: boolean, setS
   }
 
   return (
+
     <Modal scrollable={true} show={props.state} onHide={() => props.setState(false)}>
       <Modal.Header closeButton>
         <Modal.Title>Company Development Challenges</Modal.Title>
       </Modal.Header>
       
         <Modal.Body>
+
           <div className={styles.modal_buttons_div}>
             <div className={styles.modal_buttons_description_div}>
               <p>Challenges developed using <strong>React.js</strong>, <strong>Next.js</strong>, <strong>Node.js</strong> and <strong>MongoDB</strong>.</p>
-              <Button onClick={() => handleClickButtons("#dev_chall_sec_1")} variant="primary">Challenges developed on 1º/2021</Button>
+              <Button onClick={() => handleClickButtons("#dev_chall_sec_1")} className='bg-blue-700' variant="primary">Challenges developed on 1º/2021</Button>
               <hr />
             </div>
 
             <div className={styles.modal_buttons_description_div}>
               <p>Challenges developed using <strong>React.js</strong>, <strong>Next.js</strong>, <strong>Node.js</strong>, <strong>MongoDB</strong> and <strong>Expo</strong>.</p>
-              <Button onClick={() => handleClickButtons("#dev_chall_sec_2")} variant="secondary">Challenges developed on 2º/2021</Button>
+              <Button onClick={() => handleClickButtons("#dev_chall_sec_2")} className='bg-gray-600' variant="secondary">Challenges developed on 2º/2021</Button>
               <hr />
             </div>
           </div>
+
         </Modal.Body>
       
     </Modal>
+
   )
+
 }
